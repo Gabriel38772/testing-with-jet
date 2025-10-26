8a9bc24da339422a921470b59a80a39f131ff52f
Gabriel38772 <gabanxxgabbe@outlook.com>
2025-10-26 18:47:51 +0100

    chore: setup projekt (npm/webpack/jest, .gitignore)

---
+++ webpack.config.js
@@ -0,0 +1,16 @@
+const path = require('path');
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'main.js'
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [{ context: __dirname + '/src/', from: '*.html' }]
+    })
+  ],
+  mode: 'production'
+};
